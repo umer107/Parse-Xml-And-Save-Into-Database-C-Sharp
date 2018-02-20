0e1fcc86d9b0921f559313072bc2393179f2711d
MuhammadUmarWaheed <umarwaheedlog@gmail.com>
2018-02-20 15:33:12 +0500

    ParseXmlandSaveIntoDatabase

---
+++ ParseXmlSaveIntoDatabase/ParseXmlSaveIntoDatabase/Program.cs
@@ -0,0 +1,47 @@
+﻿using ParseXmlSaveIntoDatabase.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace ParseXmlSaveIntoDatabase
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            XElement xelement = XElement.Load(@"C:\Users\Umar\Desktop\sample.xml");
+            IEnumerable<XElement> parseResult = xelement.Elements();
+             Crud_DatabaseEntities1 gyt = new Crud_DatabaseEntities1();
+            //Iterate and Save Into Database
+            foreach (var item in parseResult)
+            {
+
+                System.Console.WriteLine("Item Id : {0} ,Item Name : {1}, Item Sex : {2}, Item Phone : {3}",
+                    item.Element("EmpId").Value, item.Element("Name").Value, item.Element("Sex").Value,
+                    item.Element("Phone").Value
+                    );
+                //Save Into Database
+                var message = new XmlParsing
+                {
+                    EmpId = Convert.ToInt64(item.Element("EmpId").Value),
+                    Name = item.Element("Name").Value,
+                    Sex = item.Element("Sex").Value,
+                    Phone = item.Element("Phone").Value
+                };
+
+
+                gyt.XmlParsings.Add(message);
+                gyt.SaveChanges();
+            }
+            
+            Console.ReadKey();
+        
+
+
+
+
+        }
+    }
+}
---
+++ ParseXmlSaveIntoDatabase/ParseXmlSaveIntoDatabase/XmlProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseXmlSaveIntoDatabase
+{
+    public class XmlProcess
+    {
+        
+        public long Id { get; set; }
+        public long? Employee_Id { get; set; }
+        public string Employee_Name { get; set; }
+        public string Employee_Sex { get; set; }
+        public string Employee_PhoneNo { get; set; }
+    }
+}
